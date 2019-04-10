ca5a798f2024cda1573a9a53eae81df54017b016
jesus <horus_hades@hotmail.com>
2019-04-10 17:55:55 +0200

    tslint prettier

--- src/pages/login/LoginScreen.tsx
+++ src/pages/login/LoginScreen.tsx
@@ -1,25 +1,44 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import colors from "@config/colors";
-import strings from "@config/strings";
+import * as React from 'react';
+import strings from '@config/strings';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import colors from '@config/colors';
 
 class LoginScreen extends React.Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>{strings.WELCOME_TO_LOGIN}</Text>
-      </View>
-    );
-  }
+    render() {
+        const name: string =
+            'jesus';
+        return (
+            <View
+                style={
+                    styles.container
+                }
+            >
+                <Text>
+                    {
+                        strings.WELCOME_TO_LOGIN
+                    }
+                </Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.WHITE,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const styles = StyleSheet.create(
+    {
+        container: {
+            flex: 1,
+            backgroundColor:
+                colors.WHITE,
+            alignItems:
+                'center',
+            justifyContent:
+                'center'
+        }
+    }
+);
 
 export default LoginScreen;
